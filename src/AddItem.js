c96e759ab2560ddbf0ffc2d5c680fca6cf1848aa
Rudwan Hussein <rudwa202@gmail.com>
2023-08-05 15:11:28 -0500

    functionality to handle duplicate items and dark mode

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,22 +1,52 @@
 import React, { useState } from 'react';
 
-const AddItem = ({ addItem }) => {
+const AddItem = ({ items, addItem }) => {
+  // State to track the item being added to the shopping list
   const [item, setItem] = useState('');
 
+  // State to manage the confirmation dialog for duplicate items
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  // Function to handle changes in the input field
   const handleChange = (e) => {
     setItem(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (item.trim() !== '') {
+  // Function to handle adding an item to the shopping list
+  const handleAddItem = () => {
+    // Check if the item already exists in the list
+    if (items.includes(item)) {
+      // Show the confirmation dialog for duplicate items
+      setShowConfirmation(true);
+    } else {
+      // Add the item to the shopping list and reset the input field
       addItem(item);
       setItem('');
     }
   };
 
+  // Function to handle confirming the addition of a duplicate item
+  const handleConfirm = () => {
+    // Add the item to the shopping list and reset the input field
+    addItem(item);
+    setItem('');
+    setShowConfirmation(false); // Close the confirmation dialog
+  };
+
+  // Function to handle canceling the addition of a duplicate item
+  const handleCancel = () => {
+    // Reset the input field and close the confirmation dialog
+    setItem('');
+    setShowConfirmation(false);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleAddItem();
+      }}
+    >
       <input
         type="text"
         value={item}
@@ -24,6 +54,18 @@ const AddItem = ({ addItem }) => {
         placeholder="Add item to the list"
       />
       <button type="submit">Add</button>
+      {showConfirmation && (
+        // Show the confirmation dialog if a duplicate item is being added
+        <div>
+          <p>Are you sure you want to add a duplicate item?</p>
+          <button type="button" onClick={handleConfirm}>
+            Yes
+          </button>
+          <button type="button" onClick={handleCancel}>
+            Cancel
+          </button>
+        </div>
+      )}
     </form>
   );
 };
--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { useState } from 'react';
 import ShoppingList from './ShoppingList';
 import AddItem from './AddItem';
+import './styles.css';
 
 const App = () => {
+  // State to keep track of the shopping list items
   const [items, setItems] = useState([]);
 
+  // State to track whether the app is in dark mode or light mode
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Function to add an item to the shopping list
   const addItem = (item) => {
     setItems([...items, item]);
   };
 
+  // Function to toggle between dark mode and light mode
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+    document.body.classList.toggle('dark-mode'); // Toggle dark mode class on the body element
+  };
+
   return (
     <div>
-      <AddItem addItem={addItem} />
+      {/* Button to toggle dark mode */}
+      <button onClick={toggleDarkMode}>
+        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      </button>
+
+      {/* AddItem and ShoppingList components */}
+      <AddItem items={items} addItem={addItem} />
       <ShoppingList items={items} />
     </div>
   );
 };
 
 export default App;
-
